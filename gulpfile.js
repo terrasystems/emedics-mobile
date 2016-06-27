128522ce0f533aba945b08b9d171c98ec24eea29
faust <faust@terrasystems.eu>
2016-06-27 20:09:33 +0300

    T919 add index.tpl.html for copy to index.html and inject all

--- gulpfile.js
+++ gulpfile.js
@@ -9,33 +9,52 @@ var sh = require('shelljs');
 var inject = require('gulp-inject');
 var connect = require('gulp-connect');
 var mainBowerFiles = require('main-bower-files');
+var proxy = require('http-proxy-middleware');
+var gulpCopy = require('gulp-copy');
+var rename = require('gulp-rename');
 var paths = {
   sass: ['./scss/**/*.scss']
 };
 
-gulp.task('default', ['sass','index','bwc','connect']);
+gulp.task('default', ['sass','copy' ,'index','bwc','connect']);
 
 gulp.task('connect', function () {
   connect.server({
     root: ['./www'],
     port: 9000,
     livereload: true,
+    debug:true,
     middleware: function (connect, opt) {
-      var Proxy = require('gulp-connect-proxy');
-          opt.target = 'http://ec2-52-28-6-207.eu-central-1.compute.amazonaws.com:8080/emedics-0.1.0';
-        opt.changeOrigin = true;
-        opt.route = '/rest';
-      var proxy = new Proxy(opt);
-      return [proxy];
+      return [
+        proxy('/rest', {
+          target: 'http://ec2-54-72-132-102.eu-west-1.compute.amazonaws.com:8080/emedics-0.1.0',
+          changeOrigin:true
+        })
+      ]
     }
   });
 });
 
-gulp.task('index', function () {
+gulp.task('copy', function () {
+ return  gulp.src("./www/index.tpl.html")
+  .pipe(rename("./www/index.html")).pipe(gulp.dest('.'));
+
+});
+
+gulp.task('index',['copy'], function () {
+
   var target = gulp.src('www/index.html');
-  var sources = gulp.src(['./www/js/**/*.js', './www/css/**/*.css'], {read: false});
+  var sources = gulp.src(['./www/js/**/*.js', './www/css/**/*.css']); //{read: false}
 
   return target.pipe(inject(sources,{relative: true}))
+    .pipe(gulp.dest('./www/'));
+});
+
+gulp.task('bwc',['index'], function () {
+  var target = gulp.src('www/index.html');
+  // It's not necessary to read the files (will speed up things), we're only after their paths:
+
+  return target.pipe(inject(gulp.src(mainBowerFiles(), {base: './www/lib'}), {name:'bower',relative: true}))
     .pipe(gulp.dest('./www/'));
 });
 
@@ -50,13 +69,6 @@ gulp.task('sass', function(done) {
     .pipe(rename({ extname: '.min.css' }))
     .pipe(gulp.dest('./www/css/'))
     .on('end', done);
-});
-gulp.task('bwc', function () {
-  var target = gulp.src('www/index.html');
-  // It's not necessary to read the files (will speed up things), we're only after their paths:
-
-  return target.pipe(inject(gulp.src(mainBowerFiles(), {base: './www/lib'}), {name:'bower',relative: true}))
-    .pipe(gulp.dest('./www/'));
 });
 
 gulp.task('watch', function() {
