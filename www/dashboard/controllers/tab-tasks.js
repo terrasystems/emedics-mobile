936399a7905b0fbb5cfa191e5b26bd4886ca1636
Valik Novikov <walian19947@gmail.com>
2016-06-30 16:13:17 +0300

    T931 add views of catalog and tasks

--- www/dashboard/controllers/tab-tasks.js
+++ www/dashboard/controllers/tab-tasks.js
@@ -2,72 +2,68 @@
 /*jshint -W117, -W097*/
 
 angular.module('eMedicsMobile')
-	.controller('tasksCtrl', function ($scope) {
+	.controller('tasksCtrl', function ($scope,localStorageService,http) {
 
 		var vm = this;
 		console.log("TasksCTRL");
-		$scope.data = {
-			showDelete: false
-		};
+		vm.user = localStorageService.get('userData');
+		vm.page = {};
+		vm.tasks = [];
+		vm.history = [];
 
-		$scope.edit = function(item) {
-			alert('Edit Item: ' + item.id);
-		};
-		$scope.share = function(item) {
-			alert('Share Item: ' + item.id);
-		};
 
-		$scope.moveItem = function(item, fromIndex, toIndex) {
-			$scope.items.splice(fromIndex, 1);
-			$scope.items.splice(toIndex, 0, item);
+		vm.GetAllTasks = function() {
+			http.get('private/dashboard/tasks/all')
+				.then(function (res) {
+					//blockUI.stop();
+					if (res.result) {
+						vm.tasks = res.result;
+					}
+				});
 		};
-
-		$scope.onItemDelete = function(item) {
-			$scope.items.splice($scope.items.indexOf(item), 1);
-		};
-
-		$scope.items = [
-			{ id: 0,
-			  name:'Doctor Form'
-			},
-			{ id: 1,
-				name:'Doctor Form' },
-			{ id: 2,
-				name:'Doctor Form' },
-			{ id: 3,
-				name:'Doctor Form' },
-			{ id: 4,
-				name:'Doctor Form' },
-			{ id: 5,
-				name:'Doctor Form' },
-			{ id: 6,
-				name:'Doctor Form' },
-			{ id: 7,
-				name:'Doctor Form' },
-			{ id: 8,
-				name:'Doctor Form' },
-			{ id: 9,
-				name:'Doctor Form' },
-			{ id: 10,
-				name:'Doctor Form' },
-			{ id: 2,
-				name:'Doctor Form' },
-			{ id: 3,
-				name:'Doctor Form' },
-			{ id: 4,
-				name:'Doctor Form' },
-			{ id: 5,
-				name:'Doctor Form' },
-			{ id: 6,
-				name:'Doctor Form' },
-			{ id: 7,
-				name:'Doctor Form' },
-			{ id: 8,
-				name:'Doctor Form' },
-			{ id: 9,
-				name:'Doctor Form' },
-			{ id: 10,
-				name:'Doctor Form' }
-		];
+		vm.GetAllTasks();
+		//$scope.items = [
+		//	{ id: 0,
+		//	  name:'Doctor Form'
+		//	},
+		//	{ id: 1,
+		//		name:'Doctor Form' },
+		//	{ id: 2,
+		//		name:'Doctor Form' },
+		//	{ id: 3,
+		//		name:'Doctor Form' },
+		//	{ id: 4,
+		//		name:'Doctor Form' },
+		//	{ id: 5,
+		//		name:'Doctor Form' },
+		//	{ id: 6,
+		//		name:'Doctor Form' },
+		//	{ id: 7,
+		//		name:'Doctor Form' },
+		//	{ id: 8,
+		//		name:'Doctor Form' },
+		//	{ id: 9,
+		//		name:'Doctor Form' },
+		//	{ id: 10,
+		//		name:'Doctor Form' },
+		//	{ id: 2,
+		//		name:'Doctor Form' },
+		//	{ id: 3,
+		//		name:'Doctor Form' },
+		//	{ id: 4,
+		//		name:'Doctor Form' },
+		//	{ id: 5,
+		//		name:'Doctor Form' },
+		//	{ id: 6,
+		//		name:'Doctor Form' },
+		//	{ id: 7,
+		//		name:'Doctor Form' },
+		//	{ id: 8,
+		//		name:'Doctor Form' },
+		//	{ id: 9,
+		//		name:'Doctor Form' },
+		//	{ id: 10,
+		//		name:'Doctor Form' }
+		//];
 	});
 
