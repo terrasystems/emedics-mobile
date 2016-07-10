827c2b2f57b65bc93ec21458d48099f52811862e
faust <faust@terrasystems.eu>
2016-07-10 14:20:36 +0300

    T981 preparation to offline mode  - add ripple emulator for checking online/offline mode  - add gulp task ripple

--- gulpfile.js
+++ gulpfile.js
@@ -1,103 +1,104 @@
 'use strict';
 
 var gulp = require('gulp'),
-    gutil = require('gulp-util'),
-    bower = require('bower'),
-    concat = require('gulp-concat'),
-    //sass = require('gulp-sass'),
-    minifyCss = require('gulp-minify-css'),
-    sh = require('shelljs'),
-    inject = require('gulp-inject'),
-    connect = require('gulp-connect'),
-    mainBowerFiles = require('main-bower-files'),
-    proxy = require('http-proxy-middleware'),
-    rename = require('gulp-rename'),
-    exec = require('child_process').exec,
-    paths = {
-      sass: ['./scss/**/*.scss'],
-      src:['./www/core/**/*.js','./www/public/public.core.js','./www/public/**/*.js','./www/dashboard/**/*.js',
-           './www/css/**/*.css']
-    };
+	gutil = require('gulp-util'),
+	bower = require('bower'),
+	concat = require('gulp-concat'),
+//sass = require('gulp-sass'),
+	minifyCss = require('gulp-minify-css'),
+	sh = require('shelljs'),
+	inject = require('gulp-inject'),
+	connect = require('gulp-connect'),
+	mainBowerFiles = require('main-bower-files'),
+	proxy = require('http-proxy-middleware'),
+	rename = require('gulp-rename'),
+	exec = require('child_process').exec,
+	paths = {
+		sass: ['./scss/**/*.scss'],
+		src: ['./www/core/**/*.js', './www/public/public.core.js', './www/public/**/*.js', './www/dashboard/**/*.js',
+			'./www/css/**/*.css']
+	};
 
-gulp.task('default', ['copy' ,'index','bwc']);
+gulp.task('default', ['copy', 'index', 'bwc']);
 
-gulp.task('serve', ['copy' ,'index','bwc', 'connect']);
+gulp.task('serve', ['copy', 'index', 'bwc', 'connect']);
 
 gulp.task('connect', function () {
-  connect.server({
-    root: ['./www'],
-    port: 8100,
-    livereload: true,
-    debug:false,
-    middleware: function (connect, opt) {
-      return [
-        proxy('/rest', {
-          //target: 'http://ec2-54-72-132-102.eu-west-1.compute.amazonaws.com:8080/emedics-0.1.0',
-          target:'http://192.168.20.252:8080/emedics-0.1.0',
-          changeOrigin:true
-        })
-      ];
-    }
-  });
+	connect.server({
+		root: ['./www'],
+		port: 8100,
+		livereload: true,
+		debug: false,
+		middleware: function (connect, opt) {
+			return [
+				proxy('/rest', {
+					//target: 'http://ec2-54-72-132-102.eu-west-1.compute.amazonaws.com:8080/emedics-0.1.0',
+					target: 'http://192.168.20.252:8080/emedics-0.1.0',
+					changeOrigin: true
+				})
+			];
+		}
+	});
 });
 
 gulp.task('copy', function () {
- return  gulp.src("./www/index.tpl.html")
-  .pipe(rename("./www/index.html")).pipe(gulp.dest('.'));
+	return gulp.src("./www/index.tpl.html")
+		.pipe(rename("./www/index.html")).pipe(gulp.dest('.'));
 
 });
 
-gulp.task('index',['copy'], function () {
+gulp.task('index', ['copy'], function () {
 
-  var target = gulp.src('www/index.html');
-  var sources = gulp.src(paths.src);
+	var target = gulp.src('www/index.html');
+	var sources = gulp.src(paths.src);
 
-  return target.pipe(inject(sources,{relative: true}))
-    .pipe(gulp.dest('./www/'));
+	return target.pipe(inject(sources, {relative: true}))
+		.pipe(gulp.dest('./www/'));
 });
 
-gulp.task('bwc',['index'], function () {
-  var target = gulp.src('www/index.html');
-  // It's not necessary to read the files (will speed up things), we're only after their paths:
+gulp.task('bwc', ['index'], function () {
+	var target = gulp.src('www/index.html');
+	// It's not necessary to read the files (will speed up things), we're only after their paths:
 
-  return target.pipe(inject(gulp.src(mainBowerFiles(), {base: './www/lib'}), {name:'bower',relative: true}))
-    .pipe(gulp.dest('./www/'));
+	return target.pipe(inject(gulp.src(mainBowerFiles(), {base: './www/lib'}), {name: 'bower', relative: true}))
+		.pipe(gulp.dest('./www/'));
 });
 
 
-gulp.task('ripple', ['copy' ,'index','bwc'], function () {
-  exec('ripple emulate', function (err, stdout, stderr) {});
+gulp.task('ripple', ['copy', 'index', 'bwc'], function () {
+	exec('ripple emulate', function (err, stdout, stderr) {
+	});
 });
 /*gulp.task('sass', function(done) {
-  gulp.src('./scss/ionic.app.scss')
-    .pipe(sass())
-    .on('error', sass.logError)
-    .pipe(gulp.dest('./www/css/'))
-    .pipe(minifyCss({
-      keepSpecialComments: 0
-    }))
-    .pipe(rename({ extname: '.min.css' }))
-    .pipe(gulp.dest('./www/css/'))
-    .on('end', done);
-});
+ gulp.src('./scss/ionic.app.scss')
+ .pipe(sass())
+ .on('error', sass.logError)
+ .pipe(gulp.dest('./www/css/'))
+ .pipe(minifyCss({
+ keepSpecialComments: 0
+ }))
+ .pipe(rename({ extname: '.min.css' }))
+ .pipe(gulp.dest('./www/css/'))
+ .on('end', done);
+ });
 
-gulp.task('install', ['git-check'], function() {
-  return bower.commands.install()
-    .on('log', function(data) {
-      gutil.log('bower', gutil.colors.cyan(data.id), data.message);
-    });
-});
+ gulp.task('install', ['git-check'], function() {
+ return bower.commands.install()
+ .on('log', function(data) {
+ gutil.log('bower', gutil.colors.cyan(data.id), data.message);
+ });
+ });
 
-gulp.task('git-check', function(done) {
-  if (!sh.which('git')) {
-    console.log(
-      '  ' + gutil.colors.red('Git is not installed.'),
-      '\n  Git, the version control system, is required to download Ionic.',
-      '\n  Download git here:', gutil.colors.cyan('http://git-scm.com/downloads') + '.',
-      '\n  Once git is installed, run \'' + gutil.colors.cyan('gulp install') + '\' again.'
-    );
-    process.exit(1);
-  }
-  done();
-});
+ gulp.task('git-check', function(done) {
+ if (!sh.which('git')) {
+ console.log(
+ '  ' + gutil.colors.red('Git is not installed.'),
+ '\n  Git, the version control system, is required to download Ionic.',
+ '\n  Download git here:', gutil.colors.cyan('http://git-scm.com/downloads') + '.',
+ '\n  Once git is installed, run \'' + gutil.colors.cyan('gulp install') + '\' again.'
+ );
+ process.exit(1);
+ }
+ done();
+ });
  */
