ff97e3dd602f055860c2dc96963f419a784a250d
faust <faust@terrasystems.eu>
2016-07-11 12:23:01 +0300

    T972  - add ionicModal

--- www/dashboard/controllers/tab-catalog.js
+++ www/dashboard/controllers/tab-catalog.js
@@ -2,7 +2,7 @@
 /*jshint -W117, -W097*/
 
 angular.module('core.dashboard')
-	.controller('catalogCtrl', function ( $state,localStorageService,http, alertService) {
+	.controller('catalogCtrl', function ( $state,localStorageService,http, alertService, ModalService, $scope) {
 
 		var vm = this;
 		console.log('catalogCTRL');
@@ -69,9 +69,24 @@ angular.module('core.dashboard')
 		}
 
 		vm.onLoad = function (id) {
-			alertService.showPopap('Hello' ,'' ,'dashboard/views/popups/sendTask.html').then(function (res) {
+
+				ModalService
+					.init('dashboard/views/popups/CreateTask.html', $scope)
+					.then(function(modal) {
+						modal.show();
+					});
+
+
+			/*vm.modal2 = function() {
+				ModalService
+					.init('modal2.html')
+					.then(function(modal) {
+						modal.show();
+					});
+			};*/
+			/*alertService.showPopap('Hello' ,'' ,'dashboard/views/popups/CreateTask.html').then(function (res) {
 				console.log(res);
-			});
+			});*/
 /*			http.get('private/dashboard/template/load/' + id)
 				.then(function (rest) {
 					vm.templateParams = vm.FormTemplate.find(function (form) {
@@ -139,4 +154,38 @@ angular.module('core.dashboard')
 					alertService.showAlert(res.state.message);
 				});
 		};
-	});
+	})
+	.service('ModalService', function($ionicModal, $rootScope) {
+
+
+		var init = function(tpl, $scope) {
+
+			var promise;
+			$scope = $scope || $rootScope.$new();
+
+			promise = $ionicModal.fromTemplateUrl(tpl, {
+				scope: $scope,
+				animation: 'slide-in-up'
+			}).then(function(modal) {
+				$scope.modal = modal;
+				return modal;
+			});
+
+			$scope.openModal = function() {
+				$scope.modal.show();
+			};
+			$scope.closeModal = function() {
+				$scope.modal.hide();
+			};
+			$scope.$on('$destroy', function() {
+				$scope.modal.remove();
+			});
+
+			return promise;
+		}
+
+		return {
+			init: init
+		}
+
+	})
